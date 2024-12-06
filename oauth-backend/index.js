374c83c7913e05e190fe97af25d1187611733913
anuragK-24 <24sept.anurag@gmail.com>
2024-12-06 19:51:19 +0530

    added the packages required

--- oauth-backend/index.js
+++ oauth-backend/index.js
@@ -1,7 +1,23 @@
+require("dotenv").config();
+
+const passport = require("passport");
+const session = require("express-session");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const express = require("express");
 const login = require("./routers/login");
 
 const app = express();
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.json());
 
 app.use("/api/login", login);
